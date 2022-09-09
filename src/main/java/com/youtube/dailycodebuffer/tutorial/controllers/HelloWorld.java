338d28b6d47c4868411ca29d67a73d64a799ad32
VictorEdM <thomasmannautor@gmail.com>
2022-09-09 16:16:03 -0300

    Adding spring-boot-devtools dependency

--- src/main/java/com/youtube/dailycodebuffer/tutorial/controllers/HelloWorld.java
+++ src/main/java/com/youtube/dailycodebuffer/tutorial/controllers/HelloWorld.java
@@ -1,13 +1,12 @@
 package com.youtube.dailycodebuffer.tutorial.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping()
     public String helloWorld() {
         return "Hello world";
     }
